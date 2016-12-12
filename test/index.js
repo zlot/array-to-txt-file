8cab11980869166a8cfd16e4e45c96df3b69a3cd
Mark C Mitchell <mark@markcmitchell.net>
2016-12-12 14:15:07 +1100

    add tests 'it will populate txt file with given contents' and 'it will override txt file with given name, if it does exist

--- test/index.js
+++ test/index.js
@@ -16,3 +16,21 @@ tape.test('it creates a txt file with given name, if it does not exist', t => {
     })
   })
 })
+
+tape.test('it will populate txt file with given contents', t => {
+  writeArrayToTxtFile(['👍', '✅', '😛'], testFilePath, err => {
+    t.equal(fs.readFileSync(testFilePath, 'utf-8'), '👍\n✅\n😛\n')
+    fs.unlinkSync(testFilePath) // remove test file
+    t.end()
+  })
+})
+
+tape.test('it will override txt file with given name, if it does exist', t => {
+  writeArrayToTxtFile(['a','b','c'], testFilePath, err => {
+    writeArrayToTxtFile(['d', 'e', 'f'], testFilePath, err => {
+      t.equal(fs.readFileSync(testFilePath, 'utf-8'), 'd\ne\nf\n')
+      fs.unlinkSync(testFilePath) // remove test file
+      t.end()
+    })
+  })
+})

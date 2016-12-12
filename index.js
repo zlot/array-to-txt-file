cd9536c95fd6193283e7804a30d80b8449de558f
Mark C Mitchell <mark@markcmitchell.net>
2016-12-12 14:16:28 +1100

    add index.js. All tests passing

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const _ = require('lodash')
+
+module.exports = function writeArrayToTxtFile(array, txtFilePath, cb) {
+
+  let ws = fs.createWriteStream(txtFilePath)
+  let error = null
+
+  ws.on('finish', () => {
+    if(error)
+      return
+    else
+      cb(null)
+  })
+
+  ws.on('error', err => {
+    cb(err)
+  })
+
+  try {
+    array.forEach(v => {
+      if(_.isPlainObject(v)) {
+        (_.toPairs(v)).forEach(v => {
+          ws.write(`${v}\n`)
+        })
+        return
+      }
+      ws.write(`${v}\n`)
+    })
+  } catch(ex) {
+    error = ex
+    cb(ex, null)
+  }
+
+  ws.end()
+
+}
